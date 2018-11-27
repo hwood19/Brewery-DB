aa039aef43ec3bce78d4ef1e4d085c210722336e
kimammi <44484472+kimammi@users.noreply.github.com>
2018-11-27 16:09:09 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,26 +1,31 @@
 from flask import Flask, render_template, request
 import csv
-app=Flask(__name__,static_folder='static', static_url_path='css/static.css'))
+
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+
 @app.route('/greet', methods=['POST'])
 def greet():
-    inputBrewery = request.form['beersearch']
-    inputBrewery = inputBrewery.upper()
-	with open("Brewerylist.csv") as csvfile:
-		csv_reader = csv.reader(csvfile,delimiter=',')
-		line_count = 0
-		found_result = False
-		for row in csv_reader:
-			if inputBrewery == row[0]:
-				found_result = True
-				print(row[0] + " in " + row[1] + " has a " + row[2] + " rating.")
-		
-	if found_result == False:
-		print("Specified brewery was not found.");
+    input_brewery = request.form['beersearch']
+    input_brewery = input_brewery.upper()
+    with open("Brewerylist.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        found_result = False
+        for row in csv_reader:
+            if input_brewery.upper() == row[0].upper():
+                found_result = True
+                return ("{name} in {row} has a {rating} Google rating."
+                        .format(name=row[0], row=row[1], rating=row[2]))
+
+    if not found_result:
+        return "Specified brewery was not found."
+
 
 @app.route('/')
 def home():
-    
-    return render_template("home.html",beersearch="")
+    return render_template("home.html", beersearch="")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
